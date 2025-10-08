09bb3a01188c98619b4b509a99dcc23003353083
Assane-dev14 <andaw@innovimpactafrica.com>
2025-10-08 11:25:04 +0000

    Perfectionnement

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -69,7 +69,9 @@ export class LoginComponent {
       this.loginForm.markAllAsTouched();
     }
   }
-
+navigateToHome (){
+ this.router.navigate(['/'])
+}
   private handleSuccessfulLogin(user: any) {
     console.log('Connexion réussie:', user);
     
